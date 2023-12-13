7cfc94283e3f03e5b00172aa0012c93c70c170c0
unknown <gagas112212@gmail.com>
2023-12-13 16:44:12 +0700

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        bgDark: "0D0D0D",
+        bgSoft: "333333",
+        darkBlue: "1E6F9F",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
